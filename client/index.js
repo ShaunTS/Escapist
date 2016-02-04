13a893c1cbea14897d1f48f8111dd72feae4eef3
ShaunTS <sts_0@outlook.com>
2016-02-04 15:16:24 -0500

    Created a single ajax function for fetching map objects from google api, implemented simple stack functionality for storing and fetching geo coordinates from Session

--- client/index.js
+++ client/index.js
@@ -1,41 +1,12 @@
-Template.index.onRendered(function() {
+Template.index.rendered = function() {
 
-    let mapsEnabled = !_II.getBoolean(this, 'data.enableMaps.value')
-
-    let loc = {}
-
-    loc.lat = $(this.find('.esc-map')).data('maps-lat')
-    loc.lng = $(this.find('.esc-map')).data('maps-lng')
-
-    Session.set('maps-aaa', loc)
-})
+}
 
 Template.index.helpers({
 
     requestLoadMap: function() {
-        let loc = Session.get('maps-aaa')
 
-        if(!esc.containsLatLng(loc)) return
+        esc.locStack.takeFirst('.esc-map')
 
-        esc.api.forEach(function() {
-            $.ajax({
-                url: esc.api.getMapsUrl(),
-                dataType: 'script',
-                success: function(data, status, response) {
-
-                    var map = new google.maps.Map(
-                        $('.esc-map')[0],
-                        {
-                            center: loc,
-                            zoom: 8
-                        }
-                    );
-
-                },
-                error: function(response, status, error) {
-                    _II.println('error', [response, status, error])
-                }
-            })
-        })
     }
 })
