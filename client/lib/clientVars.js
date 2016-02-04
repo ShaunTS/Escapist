82a6cbf116b10d82e98a0ede8c46154a0aa2a52c
ShaunTS <sts_0@outlook.com>
2016-02-04 15:08:43 -0500

    Added simple mechanism to client scope object that maintains a mapped list of currently active google maps js objects

--- client/lib/clientVars.js
+++ client/lib/clientVars.js
@@ -1,5 +1,22 @@
 esc.client = {}
 
-esc.client.fetchMap = function(tag) {
-    console.log("fetch > " + tag)
+esc.client.maps = []
+
+esc.client.getMap = function(mapId) {
+
+    return _.chain(esc.client.maps)
+    .where({id: mapId})
+    .pluck('mapObj')
+    .head()
+    .value()
+}
+
+esc.client.setMap = function(mapId, map) {
+    if(!_II.nonEmptyString(mapId)) return
+
+    esc.client.maps = _.reject(esc.client.maps, function(mapToken) {
+        return mapToken.id === mapId;
+    })
+
+    esc.client.maps.push({id: mapId, mapObj: map})
 }
